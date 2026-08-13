be24e004c82e40d694764634fd5a7949ef82c458
Terje Io <terjeio@users.noreply.github.com>
2026-08-13 18:49:08 +0200

    Added fix for for modal state RPM left incorrect after processing line. "Hardened" code to mostly ignore malformed/malign input, may raise alarm if really off the charts. Ref. PR#4.

--- lb_clusters.c
+++ lb_clusters.c
@@ -4,7 +4,7 @@
 
   Part of grblHAL
 
-  Copyright (c) 2022-2025 Terje Io
+  Copyright (c) 2022-2026 Terje Io
 
   grblHAL is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -36,7 +36,7 @@
 #endif
 
 #ifndef LB_SVALUE_SCALING
-#define LB_SVALUE_SCALING 0 // Change to 1 if S-values is to be multiplied by $30 value (max RPM).
+#define LB_SVALUE_SCALING 0 // Change to 1 if S-values is to be multiplied by spindle max RPM.
 #endif
 
 static struct {
@@ -44,17 +44,19 @@ static struct {
     char *s;
     char *cmd;
     char eol;
-    uint_fast16_t length;
+    bool overflow;
+    volatile uint_fast16_t length;
 } input = {0};
 
-static struct {
-    char block[34];
-    char param[24];
-    char sval[LB_CLUSTER_SIZE][10];
+static struct cluster {
+    char block[LINE_BUFFER_SIZE];
+    char param[LINE_BUFFER_SIZE + 40];
     char *s;
     char *cmd;
-    uint_fast16_t count;
-    uint_fast16_t next;
+    uint_fast8_t count;
+    uint_fast8_t next;
+    bool restore_s;
+    char sval[LB_CLUSTER_SIZE][11];
 } cluster;
 
 static stream_read_ptr file_read = NULL, stream_read = NULL;
@@ -70,7 +72,9 @@ static inline char *get_value (char *v, uint_fast8_t *offset, uint_fast16_t scal
 
     read_float(v, offset, &val);
 
-    return ftoa(val / (float)scale, 8);
+    val /= (float)scale;
+
+    return ftoa(val, val < 1.0f ? 10 : (val < 1000.0f ? 8 : 4));
 }
 
 #if LB_SVALUE_SCALING
@@ -83,7 +87,7 @@ static inline char *get_s_value (char *v)
 
     read_float(v, &cc, &val);
 
-    s = ftoa(val * settings.spindle.rpm_max, 0);
+    s = ftoa(val * gc_spindle_get(-1)->hal->rpm_max, 0);
 
     *strchr(s, '.') = input.eol;
 
@@ -91,6 +95,74 @@ static inline char *get_s_value (char *v)
 }
 
 #endif
+
+static inline int_fast8_t parse_block (void)
+{
+    if(input.length > 5 && !strncasecmp(input.block, "G1", 2) && strchr(input.block, ':') && (strchr(input.block, 'S') || strchr(input.block, 's'))) {
+
+        char c, *s = cluster.block, *s2 = input.block;
+        uint_fast8_t params = 0;
+
+        *strchr(s2, input.eol) = '\0';
+
+        while((c = *s2++)) {
+            if(c == 'S' || c == 's')
+                break;
+            if(c != ' ')
+                *s++ = c;
+        }
+        *s = '\0';
+
+        cluster.s = s + 1;
+        cluster.cmd = cluster.block;
+        strcpy(cluster.param, cluster.block);
+        cluster.count = cluster.next = 0;
+
+        s2 = strtok(s2, ":");
+        while(s2) {
+            if(cluster.count == LB_CLUSTER_SIZE)
+                return -1;
+#if LB_SVALUE_SCALING
+            strcpy(cluster.sval[cluster.count++] + 1, get_s_value(s2));
+#else
+            strcpy(cluster.sval[cluster.count] + 1, s2);
+            strcat(cluster.sval[cluster.count++], "\n");
+#endif
+            s2 = strtok(NULL, ":");
+        }
+
+        s = cluster.cmd + 3;
+        s2 = get_value(s, &params, cluster.count);
+        if(strchr(s, '\0') != s + params)
+            strcpy(cluster.param, s + params);
+        else
+            *cluster.param = '\0';
+        strcpy(s, s2);
+        cluster.s = strchr(s, '\0');
+        while(*(cluster.s - 1) == '0')
+            *(--cluster.s) = '\0';
+    }
+
+    return (int32_t)cluster.count;
+}
+
+static inline bool make_block (void)
+{
+    if(*cluster.param) {
+        strcat(strcpy(cluster.s, cluster.param), cluster.sval[0]);
+        *cluster.param = '\0';
+    } else
+        strcpy(cluster.s, cluster.sval[cluster.next]);
+
+    if(++cluster.next == cluster.count) {
+        if((cluster.restore_s = strcmp(cluster.s, cluster.sval[0])))
+            strcat(strcat(cluster.s, "G1X0"), cluster.sval[0]);
+    }
+    input.s = cluster.cmd;
+    input.length = strlen(cluster.cmd);
+
+    return cluster.next == cluster.count;
+}
 
 // File stream decoder
 
@@ -104,10 +176,21 @@ static inline void file_fill_buffer (void)
 
         input.s = s;
         input.length = 0;
+		cluster.restore_s = false;
 
         while((c = file_read()) != SERIAL_NO_DATA) {
+
+            if(++input.length >= LINE_BUFFER_SIZE - 1) {
+
+                if(!input.overflow)
+                    system_raise_alarm(Alarm_BufferOverflow);
+
+                input.overflow = true;
+
+                return;
+            }
+
             *s++ = (char)c;
-            input.length++;
             if((char)c == '\n' || (char)c == '\r') {
                 if(input.length == 1 && input.eol && input.eol != (char)c) {
                     input.eol = '\0';
@@ -121,85 +204,17 @@ static inline void file_fill_buffer (void)
 
         *s = '\0';
 
-        if(input.length > 5 && !strncasecmp(input.block, "G1", 2) && strchr(input.block, ':')) {
-
-            char *s2 = input.block, *s3;
-            uint_fast8_t params = 0;
-
-            s = cluster.block;
-
-            while((c = *s2++)) {
-                if(c != ' ')
-                    *s++ = c;
-                if(c == 'S')
-                    break;
-            }
-            *s = '\0';
-
-            cluster.s = s;
-            cluster.cmd = cluster.block;
-            cluster.count = cluster.next = 0;
-
-            s3 = s2;
-            while((c = *s2)) {
-                if(c == ':') {
-#if LB_SVALUE_SCALING
-                    *s2++ = '\0';
-                    strcpy(cluster.sval[cluster.count++], get_s_value(s3));
-#else
-                    *s2++ = input.eol;
-                    c = *s2;
-                    *s2 = '\0';
-                    strcpy(cluster.sval[cluster.count++], s3);
-                    *s2 = (char)c;
-#endif
-                    if(cluster.count == LB_CLUSTER_SIZE) {
-                        cluster.count = input.length = 0;
-                        return;
-                    }
-                    s3 = s2;
-                }
-                s2++;
-            }
-#if LB_SVALUE_SCALING
-            strcpy(cluster.sval[cluster.count++], get_s_value(s3));
-#else
-            *s2 = '\0';
-            strcpy(cluster.sval[cluster.count++], s3);
-#endif
-            s = cluster.cmd + 3;
-            s2 = get_value(s, &params, cluster.count);
-            if(strchr(s, '\0') != s + params + 1) {
-                strcpy(cluster.param, s + params);
-                *strchr(cluster.param, 'S') = input.eol;
-                *strchr(cluster.sval[0], input.eol) = '\0';
-            }
-            else
-                *cluster.param = '\0';
-            strcpy(s, s2);
-            cluster.s = strchr(s, '\0');
-            while(*(cluster.s - 1) == '0')
-                *(--cluster.s) = '\0';
-            strcat(cluster.s++, "S");
+        if(parse_block() < 0) {
+            cluster.count = input.length = 0;
+            return;
         }
     }
 
-    if(cluster.count) {
-        strcpy(cluster.s, cluster.sval[cluster.next++]);
-        if(*cluster.param) {
-            strcat(cluster.s, cluster.param);
-            *cluster.param = '\0';
-        }
-//        if(cluster.next == 2) !! oddly this slows down the parser
-//            cluster.cmd += 2;
-        if(cluster.next == cluster.count)
-            cluster.count = 0;
-        input.s = cluster.cmd;
-        input.length = strlen(cluster.cmd);
-    }
+    if(cluster.count && make_block())
+        cluster.count = 0;
 }
 
-static int32_t file_decoder (void)
+FLASHMEM static int32_t file_decoder (void)
 {
     int32_t c;
 
@@ -217,7 +232,7 @@ static int32_t file_decoder (void)
 
 // "Normal" stream decoder
 
-static int32_t stream_fill_buffer (void)
+FLASHMEM static int32_t stream_fill_buffer (void)
 {
     static char *s = NULL;
 
@@ -226,6 +241,7 @@ static int32_t stream_fill_buffer (void)
     if(s == NULL || input.s == NULL) {
         input.s = s = input.block;
         cluster.count = input.length = 0;
+        cluster.restore_s = false;
     }
 
     if(cluster.count == 0) {
@@ -243,7 +259,12 @@ static int32_t stream_fill_buffer (void)
         }
 
         if(++input.length >= LINE_BUFFER_SIZE - 1) {
-            s = NULL;
+
+            if(!input.overflow)
+                system_raise_alarm(Alarm_BufferOverflow);
+
+            input.overflow = true;
+
             return SERIAL_NO_DATA;
         }
 
@@ -262,91 +283,27 @@ static int32_t stream_fill_buffer (void)
 
         *s = '\0';
 
-        if(input.length > 5 && !strncasecmp(input.block, "G1", 2) && strchr(input.block, ':')) {
+        switch(parse_block()) {
 
-            char *s2 = input.block, *s3;
-            uint_fast8_t params = 0;
+            case -1:
+                s = NULL;
+                return SERIAL_NO_DATA;
 
-            s = cluster.block;
-
-            while((c = *s2++)) {
-                if(c != ' ')
-                    *s++ = c;
-                if(c == 'S')
-                    break;
-            }
-            *s = '\0';
-
-            cluster.s = s;
-            cluster.cmd = cluster.block;
-            strcpy(cluster.param, cluster.block);
-            cluster.count = cluster.next = 0;
-
-            s3 = s2;
-            while((c = *s2)) {
-                if(c == ':') {
-#if LB_SVALUE_SCALING
-                    *s2++ = '\0';
-                    strcpy(cluster.sval[cluster.count++], get_s_value(s3));
-#else
-                    *s2++ = input.eol;
-                    c = *s2;
-                    *s2 = '\0';
-                    strcpy(cluster.sval[cluster.count++], s3);
-                    *s2 = (char)c;
-#endif
-                    if(cluster.count == LB_CLUSTER_SIZE) {
-                        s = NULL;
-                        return SERIAL_NO_DATA;
-                    }
-                    s3 = s2;
-                }
-                s2++;
-            }
-#if LB_SVALUE_SCALING
-            strcpy(cluster.sval[cluster.count++], get_s_value(s3));
-#else
-            *s2 = '\0';
-            strcpy(cluster.sval[cluster.count++], s3);
-#endif
-            s = cluster.cmd + 3;
-            s2 = get_value(s, &params, cluster.count);
-            if(strchr(s, '\0') != s + params + 1) {
-                strcpy(cluster.param, s + params);
-                *strchr(cluster.param, 'S') = input.eol;
-                *strchr(cluster.sval[0], input.eol) = '\0';
-            }
-            else
-                *cluster.param = '\0';
-            strcpy(s, s2);
-            cluster.s = strchr(s, '\0');
-            while(*(cluster.s - 1) == '0')
-                *(--cluster.s) = '\0';
-            strcat(cluster.s++, "S");
-        } else
-            s = NULL;
+            case 0:
+                s = NULL;
+                break;
+        }
     }
 
-    if(cluster.count) {
-        strcpy(cluster.s, cluster.sval[cluster.next++]);
-        if(*cluster.param) {
-            strcat(cluster.s, cluster.param);
-            *cluster.param = '\0';
-        }
-//        if(cluster.next == 2) !! oddly this slows down the parser
-//            cluster.cmd += 2;
-        if(cluster.next == cluster.count) {
-            s = NULL;
-            cluster.count = 0;
-        }
-        input.s = cluster.cmd;
-        input.length = strlen(cluster.cmd);
+    if(cluster.count && make_block()) {
+        s = NULL;
+        cluster.count = 0;
     }
 
     return 0;
 }
 
-static int32_t stream_decoder (void)
+FLASHMEM static int32_t stream_decoder (void)
 {
     static bool buffering = true;
 
@@ -374,7 +331,7 @@ static int32_t stream_decoder (void)
 
 // Only respond with a single "ok" message for each cluster
 // or terminate cluster unpacking if error status reported.
-static status_code_t cluster_status_message (status_code_t status_code)
+FLASHMEM static status_code_t cluster_status_message (status_code_t status_code)
 {
     if(status_code != Status_OK) {
         status_message(status_code);
@@ -382,13 +339,23 @@ static status_code_t cluster_status_message (status_code_t status_code)
             input.s = NULL;
             cluster.count = cluster.next = input.length = 0;
         }
-    } else if(cluster.count == 0)
-        status_message(status_code);
+    } else if(cluster.count == 0) {
+        if(cluster.restore_s)
+            cluster.restore_s = false;
+        else
+            status_message(status_code);
+    }
 
     return status_code;
 }
 
-static void stream_changed (stream_type_t type)
+FLASHMEM static void reset_data (void)
+{
+    memset(&input, 0, sizeof(input));
+    memset(&cluster, 0, offsetof(struct cluster, sval));
+}
+
+FLASHMEM static void stream_changed (stream_type_t type)
 {
     if(on_stream_changed)
         on_stream_changed(type);
@@ -401,18 +368,18 @@ static void stream_changed (stream_type_t type)
         hal.stream.read = stream_decoder;
     }
 
-    cluster.count = cluster.next = input.length = 0;
+    reset_data();
 }
 
-static void cluster_reset (void)
+FLASHMEM static void cluster_reset (void)
 {
     if(on_reset)
         on_reset();
 
-    cluster.count = cluster.next = input.length = 0;
+    reset_data();
 }
 
-static void cluster_report (void)
+FLASHMEM static void cluster_report (void)
 {
     if(on_report_handlers_init)
         on_report_handlers_init();
@@ -421,20 +388,26 @@ static void cluster_report (void)
     grbl.report.status_message = cluster_status_message;
 }
 
-static void report_options (bool newopt)
+FLASHMEM static void report_options (bool newopt)
 {
     if(!newopt) {
         hal.stream.write("[CLUSTER:");
         hal.stream.write(uitoa(LB_CLUSTER_SIZE));
         hal.stream.write("]" ASCII_EOL);
-        hal.stream.write("[PLUGIN:LightBurn clusters v0.06]" ASCII_EOL);
+        hal.stream.write("[PLUGIN:LightBurn clusters v0.07]" ASCII_EOL);
     }
 
     on_report_options(newopt);
 }
 
-void lb_clusters_init (void)
+FLASHMEM void lb_clusters_init (void)
 {
+    uint_fast8_t idx = LB_CLUSTER_SIZE;
+
+    do {
+        *cluster.sval[--idx] = 'S';
+    } while(idx);
+
     on_stream_changed = grbl.on_stream_changed;
     grbl.on_stream_changed = stream_changed;
 
@@ -450,4 +423,4 @@ void lb_clusters_init (void)
     stream_changed(hal.stream.type);
 }
 
-#endif
+#endif // LB_CLUSTERS_ENABLE
